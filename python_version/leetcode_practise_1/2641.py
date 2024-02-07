ba86d33f0663e284750514131228ac8effdc2860
BeyonderNash <977152215@qq.com>
2024-02-07 15:46:38 +0800

    1696

---
+++ python_version/leetcode_practise_1/2641.py
@@ -0,0 +1,22 @@
+class Solution:
+    def replaceValueInTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        q = [root]
+        root.val = 0
+        while len(q) > 0:
+            q2 = []
+            sum = 0
+            for fa in q:
+                if fa.left:
+                    q2.append(fa.left)
+                    sum += fa.left.val
+                if fa.right:
+                    q2.append(fa.right)
+                    sum += fa.right.val
+            for fa in q:
+                childSum = (fa.left.val if fa.left else 0) + (fa.right.val if fa.right else 0)
+                if fa.left:
+                    fa.left.val = sum - childSum
+                if fa.right:
+                    fa.right.val = sum - childSum
+            q = q2
+        return root
