a87610ae3db81e8a1652b99392cd0bddd62186b1
BeyonderNash <977152215@qq.com>
2024-02-10 11:40:27 +0800

    0094

---
+++ cpp_version/January/0689.cpp
@@ -0,0 +1,14 @@
+﻿#include <vector>
+#include <iostream>
+class Solution {
+public:
+    long long maximumSumOfHeights(std::vector<int>& maxHeights) {
+        
+    }
+};
+
+int main()
+{
+    std::cout<<"12346";
+    return 0;
+}
