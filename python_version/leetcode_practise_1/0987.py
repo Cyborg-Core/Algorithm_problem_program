a7c58fc5f7fcd1e87f279ada514bb1f6d2026a24
BeyonderNash <977152215@qq.com>
2024-02-13 13:40:02 +0800

    0987

---
+++ python_version/leetcode_practise_1/0987.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from queue import Queue
+class Solution:
+    def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
+        q = Queue()
+        q.put((0, 0, root))
+        result = []
+        tmp_record = []
+        while not q.empty():
+            r, c, n = q.get()
+            # the child of n put into queue
+            if n.left:
+                q.put((r + 1, c - 1, n.left))
+            if n.right:
+                q.put((r + 1, c + 1, n.right))
+            tmp_record.append((r, c, n.val))
+        tmp_record = sorted(tmp_record, key=itemgetter(1,0,2))
+        column = tmp_record[0][1]
+        row = tmp_record[0][0]
+        tmp = []
+        for r,c,n in tmp_record:
+            if c != column:
+                result .append(tmp)
+                tmp = []
+                row = r
+                column = c
+            tmp.append(n)
+        result .append(tmp)
+        return result
