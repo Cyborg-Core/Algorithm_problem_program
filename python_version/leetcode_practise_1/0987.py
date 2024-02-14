e32d5ebc70da5938d8b55ebcc77ece6f53695ec8
BeyonderNash <977152215@qq.com>
2024-02-14 23:55:37 +0800

    0987

--- python_version/leetcode_practise_1/0987.py
+++ python_version/leetcode_practise_1/0987.py
@@ -1,10 +1,14 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+from operator import itemgetter
 from queue import Queue
+from typing import Optional, List
+
+
 class Solution:
     def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
         q = Queue()
