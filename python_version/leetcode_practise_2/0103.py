39950280940fa53f32cae73dbefad0659e5568b8
BeyonderNash <977152215@qq.com>
2024-02-16 09:45:57 +0800

    0103

---
+++ python_version/leetcode_practise_2/0103.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from queue import Queue
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        q = Queue()
+        q.put((0, root))
+        result = []
+        tmp_record = []
+        row = 0
+        while not q.empty():
+            r, n = q.get()
+
+            # the child of n put into queue
+            if n.left:
+                q.put((r + 1, n.left))
+            if n.right:
+                q.put((r + 1, n.right))
+
+            if r != row:
+                result.append(tmp_record)
+                row = r
+                tmp_record = []
+            tmp_record.append(n.val)
+        result.append(tmp_record)
+        return [l if i %2 ==0 else l[::-1] for i, l in enumerate(result)]
