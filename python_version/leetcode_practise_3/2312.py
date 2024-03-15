94b3749c7211c41ed45ad814b2303022e181fec9
BeyonderNash <977152215@qq.com>
2024-03-15 22:56:53 +0800

    2789

---
+++ python_version/leetcode_practise_3/2312.py
@@ -0,0 +1,24 @@
+class Solution:
+    def sellingWood(self, m: int, n: int, prices: List[List[int]]) -> int:
+        value = dict()
+
+        @cache
+        def dfs(x: int, y: int) -> int:
+            ret = value.get((x, y), 0)
+
+            if x > 1:
+                for i in range(1, x):
+                    ret = max(ret, dfs(i, y) + dfs(x - i, y))
+
+            if y > 1:
+                for j in range(1, y):
+                    ret = max(ret, dfs(x, j) + dfs(x, y - j))
+
+            return ret
+
+        for (h, w, price) in prices:
+            value[(h, w)] = price
+
+        ans = dfs(m, n)
+        dfs.cache_clear()
+        return ans
