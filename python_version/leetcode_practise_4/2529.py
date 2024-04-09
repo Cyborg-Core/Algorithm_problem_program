0afbb768491223a20b5bfbd1fb6a4f54d336a8fe
BeyonderNash <977152215@qq.com>
2024-04-09 21:11:26 +0800

    1379

---
+++ python_version/leetcode_practise_4/2529.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maximumCount(self, nums: List[int]) -> int:
+        p = 0
+        n = 0
+
+        for i in nums:
+            if i > 0:
+                p += 1
+            elif i < 0:
+                n += 1
+
+        return max(p, n)
